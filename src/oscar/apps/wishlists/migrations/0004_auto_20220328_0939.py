9bb98698e491769b99991362c7d4fde41c15b90d
Joey Jurjens <joey@highbiza.nl>
2022-04-01 14:13:43 +0200

    Use AutoField like all other models.

--- src/oscar/apps/wishlists/migrations/0004_auto_20220328_0939.py
+++ src/oscar/apps/wishlists/migrations/0004_auto_20220328_0939.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='WishListSharedEmail',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=254, verbose_name='Email')),
                 ('wishlist', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='shared_emails', to='wishlists.wishlist', verbose_name='Wish List')),
             ],
